c3337f569995ac3e8bffe7cdf876fdd7b30c02c3
Mrrrax714 <165872819+Mrrrax714@users.noreply.github.com>
2024-04-03 10:30:38 -0700

    Update clean.php
    Na

--- clean.php
+++ clean.php
@@ -2,14 +2,17 @@
 session_start();
 function scanit($dir)
 {
-$Directory = new RecursiveDirectoryIterator( $dir );
-$Iterator = new RecursiveIteratorIterator($Directory);
-$Regex = new RegexIterator($Iterator, '/^.+\.(jp*g|gif|png|webp|gif)/i', RecursiveRegexIterator::GET_MATCH);
-foreach($Regex as $f)
-{
- $fs[]=$f[0];
-}
-return $fs;
+    $Directory = new RecursiveDirectoryIterator($dir);
+    $Iterator = new RecursiveIteratorIterator($Directory);
+    $Regex = new RegexIterator(
+        $Iterator,
+        "/^.+\.(jp*g|gif|png|webp|gif)/i",
+        RecursiveRegexIterator::GET_MATCH
+    );
+    foreach ($Regex as $f) {
+        $fs[] = $f[0];
+    }
+    return $fs;
 }
 
 function deleteSmallPhotos($dir)
@@ -48,7 +51,6 @@ function deleteDuplicateFiles($dir)
         }
     }
 }
-
 
 $directory = "/storage/emulated/0/www/public/media/";
 deleteSmallPhotos($directory);
